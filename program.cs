d15da533a7f2a35bbb6bc923427c5951f921260d
arunindi <125126755+arunindi@users.noreply.github.com>
2024-04-15 11:51:42 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator mine=new Calculator();
+        {
+            Console.Write(mine.Add(2,3));               
+        }
+    }
+}
+public class Calculator
+{    
+    public int Add(int a,int b)
+   {
+    return a+b;
+   }
+}
